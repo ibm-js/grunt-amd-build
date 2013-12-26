805fb682184b517eab0a583f1b7953ab5eb224b1
Clément Mathieu <clement.mathieu@outlook.com>
2013-12-26 07:26:08 +0100

    add basic plugin support

---
+++ tasks/lib/config.js
@@ -0,0 +1,71 @@
+"use strict";
+
+module.exports = (function () {
+	var eachProp = require("./lang").eachProp,
+		defaultConfig = {
+			baseUrl: "./",
+			dir: "dist/",
+			optimize: "none",
+			packages: [],
+			map: {},
+			paths: {},
+			layers: {}	
+		},
+		addTrailingSlash = function (string) {
+			if (string.charAt(string.length - 1) !== '/') {
+					return string + '/';
+			}
+			return string;
+		};
+		
+	
+    return {
+		normalize: function(cfg){
+			var config = defaultConfig;
+			
+			//Copy the cfg value to config except for layers which need special treatment
+			eachProp(cfg, function (prop, value) {
+				if (prop != "layers") {
+					config[prop] = value;
+				}
+			});
+			
+			//Make sure the baseUrl and the output dir end in a slash.
+			config.baseUrl = addTrailingSlash(config.baseUrl);
+			config.dir = addTrailingSlash(config.dir);
+			
+			
+            //Adjust packages if necessary.
+            if (cfg.packages) {
+                config.pkgs = {};
+
+				cfg.packages.forEach(function (pkg) {
+                    pkg = typeof pkg === 'string' ? {name: pkg} : pkg;
+
+					config.pkgs[pkg.name] = {
+						name: pkg.name,
+						location: pkg.location || pkg.name,
+						main: pkg.main || "main"
+					};
+                });
+			}
+			
+			//Add layers.
+            if (cfg.layers) {
+                eachProp(cfg.layers, function (layerName, layerObj) {
+					var exclude = (layerObj.exclude || []).slice(0),
+						include = (layerObj.include || []).slice(0);
+					if(include.indexOf(layerName) < 0){
+						include.push(layerName);
+					}
+					config.layers[layerName] = {
+						include: include,
+						exclude: exclude,
+						outputPath: config.dir + layerName + ".js"
+					};
+                });
+			}
+			return config;
+		}
+	};	
+})();
---
+++ tasks/lib/lang.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = (function () {
+	
+	return {
+		getOwn: function (obj, prop) {
+			return obj.hasOwnProperty(prop) && obj[prop];
+		},
+		eachProp: function (obj, func) {
+			var prop;
+			for (prop in obj) {
+				if (obj.hasOwnProperty(prop)) {
+					func(prop, obj[prop]);
+				}
+			}
+		}
+	};
+})();
